ade311ff151eb34a0ac5c67527b471e4083e84a6
CorpMail <rodrigo.urrutia@rutiandco.com>
2025-01-05 15:04:55 -0300

    Asegurar que register-new-business almacene JSON en la base de datos

--- src/endpoints/register-new-business.js
+++ src/endpoints/register-new-business.js
@@ -6,9 +6,18 @@ export default async function postRegisterNewBusiness({
     const query = `
         INSERT INTO companies (id, fantasy_name, legal_info, url_domain, industrial, head_quarter, branches, modules)
         VALUES  ($1, $2, $3::jsonb, $4, $5::jsonb, $6::jsonb, $7::jsonb, $8::jsonb)
+        RETURNING id, fantasy_name, legal_info, url_domain, industrial, head_quarter, branches, modules;
         ;
     `;
-    const params = [id, fantasy_name, legal_info, url_domain, industrial, head_quarter, branches, modules];
+    const params = [
+        id,
+        fantasy_name,
+        legal_info,
+        url_domain,
+        JSON.stringify(industrial),
+        JSON.stringify(head_quarter),
+        JSON.stringify(branches),
+        JSON.stringify(modules)];
     try {
         const result = await queryDb(query, params, false);
         console.log(result);
